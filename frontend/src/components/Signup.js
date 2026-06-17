7973daa99955d1fa77c42a430d59858d88b80eb7
Rajesh L <rajeshfaster817@gmail.com>
2026-06-17 22:02:32 +0530

    Update Signup.js

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -25,7 +25,7 @@ const Signup = () => {
         }
 
         try {
-            const response = await fetch('http://localhost:5000/signup', {
+            const response = await fetch("https://truthshield-a-fake-news-detection-system.onrender.com/signup", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
