bc39d739d8619134e1c788cfbb0d24060966923e
Rajesh L <rajeshfaster817@gmail.com>
2026-06-17 22:01:23 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:5000/login', {
+            const response = await fetch("https://truthshield-a-fake-news-detection-system.onrender.com/login", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
